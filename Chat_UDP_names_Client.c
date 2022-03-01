dc159e06c1157f3ed8be2cc12a7cb86d708841e4
915-Pop-Calin <72080637+915-Pop-Calin@users.noreply.github.com>
2022-03-01 15:29:19 +0200

    Comments + Bugfixing
    Comments and bugfixes done

--- Chat_UDP_names_Client.c
+++ Chat_UDP_names_Client.c
@@ -7,110 +7,153 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #define SERVER_PORT 6667
+#define USERNAME_LENGTH 32
+#define MESSAGE_SIZE 32
 #define IP_ADDRESS "10.152.4.78"
+//#define IP_ADDRESS "192.168.1.2"
 #define MAX_USERS 100
 #include <stdbool.h>
 
+// an user has its address and a string representing its name
 typedef struct user{
 	struct sockaddr_in address;
-	char name[32];
+	char name[USERNAME_LENGTH];
 }user;
 
+// check whether in a list of users, a given user is found
 bool is_found(struct user* users, struct user current_user){
 	struct sockaddr_in address = current_user.address;
-	for (int i = 0; i < 100; i++){
+	// store here the address we are looking for
+	for (int i = 0; i < MAX_USERS; i++){
 		struct sockaddr_in cur_addr = users[i].address;
+		// store the current address in this struct
 		if (cur_addr.sin_port == address.sin_port && cur_addr.sin_addr.s_addr == address.sin_addr.s_addr){
 			return true;
 		}
+		// if the current address has the same port and ip address as the one we are looking for, we have found it, return true
 	}
 	return false;
+	// if we can't find it, return true
 }
 
+// remove a user from a vector of users if we can find it, or do nothing if we don't find it
 void remove_user(struct user* users, int size, struct user current_user){
 	struct sockaddr_in address = current_user.address;
+	// store here the address we are looking forward to remove
 	for (int i = 0; i < size; i++){
 		struct sockaddr_in cur_addr = users[i].address;
+		// store here the current address
 		if (cur_addr.sin_port == address.sin_port && cur_addr.sin_addr.s_addr == address.sin_addr.s_addr){
+			// if the current address has the same port and ip address as the one we are looking to remove
 			users[i] = users[size - 1];
+			// the last user in the vector is moved into the current position
 			memset(&users[size - 1], 0, sizeof(users[size - 1]));
+			// and it is zero'd so we know there's nothing there
 			return;
 		}
 
 	}
-	//perror("remove");
-
 }
 
-
+// return a user by its username, or a zeroed one if we don't find it
 struct user find_user_by_name(user* users, char* username){
 		unsigned int current_ip = 1, current_port = 1;
+		// we store the current port and the current ip in those variables
+		// so we know to break out of the while when they are zero
+		// we can do this due to how we manipulate the vector
 		int i = 0;
+		// set i to be the first position
 
-		while ((current_ip != 0 || current_port != 0) && i < 100){
+		while ((current_ip != 0 || current_port != 0) && i < MAX_USERS){
 			struct sockaddr_in cur_addr = users[i].address;
+			// store here the current address
 			current_ip = cur_addr.sin_addr.s_addr;
 			current_port = cur_addr.sin_port;
+			// we store the current ip and port
 			char* name = users[i].name;
 
+			// if the names coincide stringwise, return the current user
 			if (strcmp(username, name) == 0){
 				return users[i];
 			}
-
+			// otherwise go forward
 			i += 1;
 
 		}
 
+		// if we reached this point, we haven't found a user so we return a zero'd one
 		struct user current_user;
 		memset(&current_user, 0, sizeof(current_user));
 		return current_user;
 
 }
 
+// return a user's username by its address which uniquely identifies it, or return NULL if we don't find
 char* find_user_by_address(user* users, struct sockaddr_in address){
+
+	// this works similarly to the above function	
 	unsigned int current_ip = address.sin_addr.s_addr, current_port = address.sin_port;
 	int i = 0;
 	
-	while ((current_ip != 0 || current_port != 0) && i <  100){
+	while ((current_ip != 0 || current_port != 0) && i <  MAX_USERS){
 		struct sockaddr_in cur_addr = users[i].address;
 		
+		// if the addresses coincide, we return the name of the user
 		if (cur_addr.sin_port == current_port && cur_addr.sin_addr.s_addr == current_ip){
 			return users[i].name;
 		}
 		i += 1;
 	}
+
+	// otherwise we return NULL
 	return NULL;
 
 }
 
+// this gives a string and processes it 
 void process_string(char* buf, struct user* users, int udp_socket){
+		// this will be the save point for our strtok
 		char** save_point = (char**) malloc(sizeof(char*));
 
+		// this goes until the first space, exclusively
+		// save point will point to the place after the space
 		char* command = strtok_r(buf, " ",  save_point);
+		
+		// this goes until the first semi-colon, exclusively
+		// save point will point to the place after the semi-colon
 		char* user_name = strtok_r(*save_point, ":", save_point);
+		
+		// the message will be everything after the semi-colon
 		char* message = *save_point;	
+
+		// we may free the save point as we don't need it anymore
 		free(save_point);
 		
-
+		// this should be the whisper command, the only one implemented as of yet
 		char whisper_command[3] = "\\w\0";
+
+		// if the given command is not a valid command, we let them know and return
 		if (strcmp(command, whisper_command) != 0){
 			printf("Invalid command!\n");
 			return;
 		}
 		
-
+		// now we find the user to whom we want to send the message 
+		// if we don't find it, we print a message and return
 		struct user current_user = find_user_by_name(users, user_name);
 		if (*((unsigned long int*) &current_user.address) == 0){
 			printf("We couldn't find the user %s\n", user_name);
 			return;
 		}
 
+		// send status will contain the status of our message
 		int send_status;
 		struct sockaddr_in current_address = current_user.address;
-
-
+		
+		// we send to the user our message
 		send_status = sendto(udp_socket, message, 32, 0, (struct sockaddr*) &current_address, sizeof(current_address));
 		
+		// depending on the status, we send a message
 		if (send_status < 0){
 			printf("Message couldn't be sent. Please retry\n");
 		}
@@ -120,12 +163,31 @@ void process_string(char* buf, struct user* users, int udp_socket){
 
 }
 
+void read_string_safely(char* string, int maximum_length){
+	int current_position = 0;
+	int current_character;
+	do{
+	    current_character = getc(stdin);
+	    if (current_character != '\n'){
+	        string[current_position] = current_character;
+	        current_position++;
+	    }
+	}
+	while (current_character != '\n' && current_position < maximum_length);
+}
 
 int main(int argc, char** argv){
-	struct hostent* h;
-	h = gethostbyname(IP_ADDRESS);
+	// we read the username of our current user
+	char user_name[USERNAME_LENGTH] = {0};
+	printf("Your username is:\n");
+	read_string_safely(user_name, USERNAME_LENGTH - 1);
+	
+	// get the hostent which has our ip address
+	struct hostent* h = gethostbyname(IP_ADDRESS);
+
 	// we create an UDP socket for the clients to communicate in
 	int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	// if it failed, we exit
 	if (udp_socket < 0){
 		perror("udp socket");
 		exit(1);
@@ -136,43 +198,69 @@ int main(int argc, char** argv){
 	socklen_t addr_len = sizeof(cli_addr);
 	getsockname(udp_socket, (struct sockaddr*)  &cli_addr, &addr_len);
 	
+	// we create a tcp socket with which we will communicate with the server
 	int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// if it failed, we exit
 	if (tcp_socket < 0){
 		perror("tcp socket");
 		exit(1);
 	}
+	
+	// this will store the address of the server
+	// our life was made easier by finding the host
 	struct sockaddr_in serv_addr;
-
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = h->h_addrtype;
 	memcpy((char*) &serv_addr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
 	serv_addr.sin_port = htons(SERVER_PORT);
 
+	// we try to connect through tcp to the server
 	int connect_status;
 	connect_status = connect(tcp_socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	// if we couldn't connect, we exit
 	if (connect_status < 0){
 		perror("connect");
 		exit(1);
 	}
 	
-	char user_name[32] = {0};
-	printf("Your username is:\n");
-	scanf("%s", user_name);
+	// we send the username through the udp_socket, which will give the server our address as well
+	sendto(udp_socket, user_name, USERNAME_LENGTH, 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 
-	sendto(udp_socket, user_name, 32, 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
-
-	int number_of_clients;
+	// we create an array of users of size at most MAX_USERS and we set everything to 0
+	int number_of_clients, clients_netw;
 	struct user users[MAX_USERS];
 	for (int i = 0; i < MAX_USERS; i++){
 		memset(&users[i], 0, sizeof(serv_addr));		
 	}
 
-	read(tcp_socket, &number_of_clients, sizeof(number_of_clients));
+	int status_netw, status;
+	read(tcp_socket, &status_netw, sizeof(status_netw));
+	status = ntohl(status_netw);
+
+	if (status == 1){
+	    printf("Chatroom is full!\n");
+	    close(udp_socket);
+	    close(tcp_socket);
+	    exit(1);
+	}
+
+	if (status == -1){
+	    printf("Name is already is taken!\n");
+        close(udp_socket);
+        close(tcp_socket);
+	    exit(-1);
+	}
+
+
+	// we read from the tcp_socket 
+	read(tcp_socket, &clients_netw, sizeof(clients_netw));
+	number_of_clients = ntohl(clients_netw);
 	for (int i = 0; i < number_of_clients; i++){
 		read(tcp_socket, &users[i], sizeof(users[i]));
 		printf("I have just read user %s\n", users[i].name);
 	}
 
+	// file descriptors for the select function
 	fd_set read_fds, master;
 
 	FD_ZERO(&read_fds);
@@ -181,73 +269,78 @@ int main(int argc, char** argv){
 	FD_SET(0, &master);
 	FD_SET(udp_socket, &master);
 
-	int fd_max;
-	if (tcp_socket > udp_socket){
-		fd_max = tcp_socket;
-	}
-	else{
-		fd_max = udp_socket;
-	}
-
+	// this will store the maximum out of the file descriptors
+	int fd_max = (tcp_socket < udp_socket) ? udp_socket : tcp_socket;
+	
+	// we print the format of the message so the user knows
 	printf("\\w <name>:<message>\n");
 
+	// we do an infinite loop
 	for (;;){
+		// we try to check for new users
 		read_fds = master;
 		if (select(fd_max + 1, &read_fds, NULL, NULL, NULL) == -1){
 			perror("select");
 			exit(1);
 		}
-		// make this do background work, we can use threads for ex	
 		
+		// if this is true, then we have a new user from the server
+		// because only with the server we communicate on TCP
 		if (FD_ISSET(tcp_socket, &read_fds)){
 			struct user new_user;
 			int read_status;
 			read_status = read(tcp_socket, &new_user, sizeof(new_user));
 			
+			// depending on whether the user already exists, we want to add or remove it
 			if (read_status > 0){
-			if (!is_found(users, new_user)){
-				printf("I have added user %s\n", new_user.name);
-				users[number_of_clients] = new_user;
-				number_of_clients++;
-			}
-			else{
-				printf("I have removed user %s\n", new_user.name);
-				remove_user(users, number_of_clients, new_user);
-				number_of_clients--;	
-			}
+				if (!is_found(users, new_user)){
+					printf("I have added user %s\n", new_user.name);
+					users[number_of_clients] = new_user;
+					number_of_clients++;
+				}
+				else{
+					printf("I have removed user %s\n", new_user.name);
+					remove_user(users, number_of_clients, new_user);
+					number_of_clients--;	
+				}
 			}
 			
+			// if we can't read, that means server has closed connection
 			else{
 				printf("Server has closed connection...\n");
 				break;
 			}
 		}
 
-
+		// if we can read from the console, that means a message is trying to be sent
+		// so we try to process it
 		if (FD_ISSET(0, &read_fds)){
-			char buf[256];
-			memset(buf, 0, sizeof(buf));
-			read(0, buf, sizeof(buf));
-			process_string(buf, users, udp_socket);
+			char buffer[MESSAGE_SIZE] = {0};
+			read_string_safely(buffer, MESSAGE_SIZE - 1);
+			process_string(buffer, users, udp_socket);
 		}
 
+		// if we can read from the UDP sockets, then a message was received from another client
+		// so we try to see the message and print it to the console
 		if (FD_ISSET(udp_socket, &read_fds)){
-			char buf[32] = {0};
+			char buf[MESSAGE_SIZE] = {0};
 			struct sockaddr_in sender_addr;
 			socklen_t addr_len = sizeof(sender_addr);
-			recvfrom(udp_socket, buf, 32, 0, (struct sockaddr*) &sender_addr, &addr_len); 
+			recvfrom(udp_socket, buf, MESSAGE_SIZE, 0, (struct sockaddr*) &sender_addr, &addr_len);
 			char* user_name = find_user_by_address(users, sender_addr);
 			
 			if (user_name == NULL){
 				printf("Invalid user!\n");
 			}
 			else{
-				printf("from %s:%s", user_name, buf);
+				printf("from %s:%s\n", user_name, buf);
 			}
 		}
 
 	}
 	
+	// we close the sockets, but due to the structure we can't reach there
+	// we will try to change that
 	close(udp_socket);
 	close(tcp_socket);
 
--- Chat_UDP_names_Server.c
+++ Chat_UDP_names_Server.c
@@ -7,7 +7,9 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-
+#define MAX_SIZE 100
+#define MESSAGE_SIZE 32
+#define USERNAME_SIZE 32
 #define SERVER_PORT 6667
 
 fd_set master;
@@ -23,46 +25,71 @@ socklen_t addr_len;
 int j, crt, client_count = 0;
 int udp_socket;
 
+// an user has an address and a string which represents its name
 typedef struct user{
 	struct sockaddr_in address;
 	char name[32];
 }user;
 
+// we define a key value pair which will be stored by the map
 typedef struct key_value_pair{
 	int key;
 	struct user value;
 }key_value_pair;
 
+// we store the map as an array of key value pairs because I am too lazy to
+// implement a hash map at this point
 typedef struct map{
-	key_value_pair items[100];
+	key_value_pair items[MAX_SIZE];
 	int size;
 }map;
 
+// this global variable will represent all of our users
+// the keys are the respective TCP sockets
+// the values are their user structs
 map elements;
 
+// we initialise our map by zero'ing each element
 void initialise_map(map* current_map){
-	for (int i = 0; i < 100; i++){
+	for (int i = 0; i < MAX_SIZE; i++){
 		memset(&current_map->items[i], 0, sizeof(current_map->items[i]));
 	}
 }
 
-void add_pair(map* current_map, int current_key, struct user current_value){
+// we add a pair to the end of the vector
+// 0 is success, 1 means chatroom is full, -1 means name already taken
+int add_pair(map* current_map, int current_key, struct user current_value){
+    if (current_map->items[MAX_SIZE - 1].key != 0){
+        printf("Chat is full! Wait for someone to leave...\n");
+        return 1;
+    }
+    for (int i = 0; i < current_map->size; i++){
+        if (strcmp(current_value.name, current_map->items[i].value.name) == 0){
+            printf("This name is already taken!\n");
+            return -1;
+        }
+    }
 	struct key_value_pair pair;
 	pair.key = current_key;
 	pair.value = current_value;
+	// add the pair at the end of our map
 	current_map->items[current_map->size] = pair;
 	current_map->size++;
-	// check for an update
+	return 0;
 }
 
-
+// we remove a key value pair from the map
 void remove_key(map* current_map, int current_key){
-	for (int i = 0; i < 100; i++){
+	for (int i = 0; i < current_map->size; i++){
 		if (current_key == current_map->items[i].key){
+			// if we found our current element
 			struct user zero_addr;
 			memset(&zero_addr, 0, sizeof(zero_addr));
 			
+			// we set our current element to be the last element
 			current_map->items[i] = current_map->items[current_map->size - 1];
+
+			// we set the last element to be zero'd
 			current_map->items[current_map->size - 1].key = 0;
 			current_map->items[current_map->size - 1].value = zero_addr;
 			current_map->size--;
@@ -72,16 +99,20 @@ void remove_key(map* current_map, int current_key){
 
 }
 
+// we find the value represented by a value
 struct user find_value(map* current_map, int current_key){
+	// we zero the current value in case we don't find anything
 	struct user current_value;
 	memset(&current_value, 0, sizeof(current_value));
 	for (int i = 0; i < current_map->size; i++){
 		if (current_map->items[i].key == current_key){
+			// if we found a match, we return it
 			return current_map->items[i].value;
 		}
 	}
 	return current_value;
 }
+
 
 struct sockaddr_in get_socket_name(int socket, bool is_client){
 	struct sockaddr_in addr;
@@ -113,21 +144,28 @@ int get_port(int socket, bool is_client){
 	return addr.sin_port;
 }
 
+
 void send_all_addresses_to_new_client(int fdn){
-        int total_clients = client_count - 1;
-        send(new_fd, &total_clients, sizeof(total_clients), 0);
-	
+	// we send to our new client the number of clients bar him
+    int total_clients = client_count - 1;
+        // I actually don't know why the next line worked
+	// it totally should not
+	// send(new_fd, &total_clients, sizeof(total_clients), 0);
+	int clients_netw = htonl(total_clients);
+	send(fdn, &clients_netw, sizeof(clients_netw), 0);
+
 	int cur_index = 0;
 	unsigned int cur_ip = elements.items[cur_index].value.address.sin_addr.s_addr;
 	unsigned int cur_port = elements.items[cur_index].value.address.sin_port;
 	int cur_key;
 
-	while ((cur_ip != 0 || cur_port != 0) && cur_index < 100){
+	while ((cur_ip != 0 || cur_port != 0) && cur_index < MAX_SIZE){
 		struct user cur_user = elements.items[cur_index].value;
 		cur_ip = cur_user.address.sin_addr.s_addr;
 		cur_port = cur_user.address.sin_port;
 		cur_key = elements.items[cur_index].key;
 
+		// send the current_user if we haven't reached the end and if we're not at our current client
 		if ((cur_ip != 0 || cur_port != 0) && cur_key != fdn)
 			send(fdn, &cur_user, sizeof(cur_user), 0);
 		cur_index++;
@@ -137,13 +175,15 @@ void send_all_addresses_to_new_client(int fdn){
 }
 
 void send_new_address_to_all_clients(int fdn){
-
+	// we send to all clients the address of our new client
 	int cur_index = 0;
+	// this will contain our new user
 	struct user cur_user = find_value(&elements, fdn);
 	int cur_key = 1;	
 
-	while (cur_key != 0 && cur_index < 100){
+	while (cur_key != 0 && cur_index < MAX_SIZE){
 		cur_key = elements.items[cur_index].key;
+		// if we haven't reached the end and if we're not at our current user, we send it
 		if (cur_key != 0 && cur_key != fdn)
 			send(cur_key, &cur_user, sizeof(cur_user), 0);
 		cur_index++;
@@ -153,51 +193,81 @@ void send_new_address_to_all_clients(int fdn){
 }
 
 
-
+// we handle a new client here
 void handle_new_client(){
+	// accept a new connection
 	addr_len = sizeof(remote_addr);
 	if ((new_fd = accept(listener, (struct sockaddr*) &remote_addr, (socklen_t*) &addr_len)) == -1){
 		perror("accept");
 	}
 	else{
-		FD_SET(new_fd, &master);
-		if (new_fd > fd_max){
-			fd_max = new_fd;
-		}
-		printf("selectserver: new connection from %s on socket %d\n", get_IP_address(new_fd, false), new_fd);
+
 		client_count += 1;
 		struct sockaddr_in current_address;
 		socklen_t length = sizeof(current_address);
 		
-		char msg[32] = {0};
-		recvfrom(udp_socket, msg, 32, 0, (struct sockaddr*) &current_address, &length);
+		// we receive the name of our client
+		char msg[MESSAGE_SIZE] = {0};
+		recvfrom(udp_socket, msg, MESSAGE_SIZE, 0, (struct sockaddr*) &current_address, &length);
 			
+		// we create our current struct
 		struct user new_user;
 		new_user.address = current_address;
-		memset(new_user.name, 0, 32);
+		memset(new_user.name, 0, USERNAME_SIZE);
 		memcpy(new_user.name, msg, strlen(msg));
 
-		add_pair(&elements, new_fd, new_user);	
-		send_all_addresses_to_new_client(new_fd);
-		send_new_address_to_all_clients(new_fd);
+		// we add it to the elements and we do the sends both ways
+		int status = add_pair(&elements, new_fd, new_user);
+		int sent_status = htonl(status);
+
+		send(new_fd, &sent_status, sizeof(sent_status), 0);
+		if (status == 0) {
+            // add a new fd to the set
+            FD_SET(new_fd, &master);
+            // if it's the maximum descriptor, we set it as such
+            if (new_fd > fd_max){
+                fd_max = new_fd;
+            }
+            printf("selectserver: new connection from %s on socket %d\n", get_IP_address(new_fd, false), new_fd);
+            // we increment the client count
+            send_all_addresses_to_new_client(new_fd);
+            send_new_address_to_all_clients(new_fd);
+        }
+		else{
+		    close(new_fd);
+		    printf("Server failed to connect\n");
+		}
 	}
 }
 
+// we handle the exit of a client there
 void handle_exit(int socket){
+	// we see how many bytes we receive as answer
 	nbytes = recv(socket, buf, sizeof(buf), 0);
+
+	// 0 bytes means the client forcibly hung up
 	if (nbytes == 0){
 		printf("<selectserver>: client %d forcibly hung up\n", socket);
 	}
+	
+	// <0 bytes means there was a problem with the receive function
 	if (nbytes < 0){
 		perror("recv");
 	}
+	
+	// >0 bytes means our client told us goodbye nicely
 	if (nbytes > 0){
 		printf("<selectserver>: client %d told us nicely he's leaving\n", socket);
 	}
+
+	// we tell all clients that the client does not exist anymore
 	send_new_address_to_all_clients(socket);	
 	// use free
+	//
+	// and we remove the key
 	remove_key(&elements, socket);
 	
+	// decrement the client count, close the socket and clear it from the master set
 	client_count--;
 	close(socket);
 	FD_CLR(socket, &master);
@@ -206,53 +276,69 @@ void handle_exit(int socket){
 
 
 int main(int argc, char** argv){
+	// we zero the master set and read_fds
 	FD_ZERO(&master);
 	FD_ZERO(&read_fds);
 
+	// if we couldn't create a listener TCP socket, we exit
 	if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("socket");
 		exit(1);
 	}
 	
+	// if we can't set the options to reuse the address, then we exit 
 	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){
 		perror("setsockopt");
 		exit(1);
 	}
+
+	// we create the server address
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(SERVER_PORT);
+
+	// we bind the listener to our address
 	if (bind(listener, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1){
 		perror("bind");
 		exit(1);
 	}
 
+	// we create a udp socket and if we fail we exit
 	if ((udp_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
 		perror("socket");
 		exit(1);
 	}
 
+	// we bind the udp socket to the server address as well
 	if (bind(udp_socket, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1){
 		perror("bind");
 		exit(1);
 	}
 
+	// and we listen for 10 connections, and if it fails we exit
 	if (listen(listener, 10) == -1){
 		perror("listen");
 		exit(1);
 	}
 
+	// we have only one element in the master set, that is the listener, thus it is the max
 	FD_SET(listener, &master);
-
 	fd_max = listener;
 
+	// infinite loop
 	for (;;){
+		// save the master set
 		read_fds = master;
+
+		// if we don't manage to select, there was an error and we exit
 		if (select(fd_max + 1, &read_fds, NULL, NULL, NULL) == -1){
 			perror("select");
 			exit(1);
 		}
 
+		// if there's a new message on the listener, that means there's a new client and we need to handle it
+		// otherwise, we need to handle its exit 
 		for (int i = 0; i <= fd_max; i++){
 			if (FD_ISSET(i, &read_fds)){
 				crt = i;
